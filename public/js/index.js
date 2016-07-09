8b81dab70e8a2b6b46a6777df9621d64bcf6f872
Themis Theologitis <theothem@csd.uoc.gr>
2016-07-09 17:36:58 +0300

    portfolio

---
+++ public/js/index.js
@@ -0,0 +1,207 @@
+$(window).load(function(){
+  scroll();
+  carousel();
+});
+
+function scroll() {
+  var $window = $(window);
+  var scrollTime = 1.1;
+  var scrollDistance = 300;
+
+  $window.on("mousewheel DOMMouseScroll", function(event){
+
+    event.preventDefault(); 
+
+    var delta = event.originalEvent.wheelDelta/120 || -event.originalEvent.detail/3;
+    var scrollTop = $window.scrollTop();
+    var finalScroll = scrollTop - parseInt(delta*scrollDistance);
+
+    TweenMax.to($window, scrollTime, {
+      scrollTo : { y: finalScroll, autoKill:true },
+        ease: "Expo.easeInOut",
+        overwrite: 5              
+      });
+
+  });
+}
+
+
+function carousel(argument) {
+  var carousel = (function () {
+
+    var activeID = 0,
+        itemW = document.getElementById("carousel_container").offsetWidth,
+        itemH = document.getElementsByClassName("carousel_item")[0].offsetHeight,
+        carousel_count = $('.carousel_item').length,
+        $carouselItems = $('.carousel_items'),
+        $carouselItem = $('.carousel_item'),
+        $slideShow__slideTitle = $('.slideShow__slideTitle'),
+        $slideShow__slideSubTitle = $('.slideShow__slideSubTitle'),
+        $arrowPrev = $('.item_prev'),
+        $arrowNext = $('.item_next'),
+        $itemArrow = $('.item_arrow'),
+        $navDot,
+        $navDots = $('.nav_dots'),
+        swipeDir,
+        slideSpeed = .45,
+        slideMeth = Power2.EaseInOut;
+    
+    $(window).resize(function() {
+      activeID = 0;
+      itemW = document.getElementById("carousel_container").offsetWidth;
+      itemH = document.getElementsByClassName("carousel_item")[0].offsetHeight;
+      carousel_count = $('.carousel_item').length;
+      $carouselItems = $('.carousel_items');
+      $carouselItem = $('.carousel_item');
+      $slideShow__slideTitle = $('.slideShow__slideTitle');
+      $slideShow__slideSubTitle = $('.slideShow__slideSubTitle');
+      $arrowPrev = $('.item_prev');
+      $arrowNext = $('.item_next');
+      $itemArrow = $('.item_arrow');
+      $navDot;
+      $navDots = $('.nav_dots');
+      swipeDir;
+      slideSpeed = .45;
+      slideMeth = Power2.EaseInOut;
+      resize();
+    });
+
+    function init() {
+       
+      $carouselItems.css({'width': (itemW * carousel_count) + 'px'});
+      $navDots.css({'width': (25 * carousel_count) + 'px'});
+      $carouselItem.css({'width': itemW+'px'});
+      $itemArrow.css({'opacity': .8});
+      setupDraggable();
+      setupDots();
+      navigateSlide();
+
+      var title         =  $('.slideShow__slideTitle')[activeID];
+      var subTitle      =  $('.slideShow__slideSubTitle')[activeID];
+      
+      var tl            = new TimelineLite(); 
+      tl.fromTo(title, 0.6, {top:200, opacity: 0},{top:340, opacity:.5}, '-=0.3')  
+        .fromTo(subTitle, 0.6, {top:200, opacity: 0},{top:420, opacity:.5}, '-=0.3')
+    }
+
+    function resize() {
+       
+      $carouselItems.css({'width': (itemW * carousel_count) + 'px'});
+      $navDots.css({'width': (25 * carousel_count) + 'px'});
+      $carouselItem.css({'width': itemW+'px'});
+      $itemArrow.css({'opacity': .8});
+      setupDraggable();
+      navigateSlide();
+
+      var title         =  $('.slideShow__slideTitle')[activeID];
+      var subTitle      =  $('.slideShow__slideSubTitle')[activeID];
+      
+      var tl            = new TimelineLite(); 
+      tl.fromTo(title, 0.6, {top:200, opacity: 0},{top:(itemH - (itemH/3)-100), opacity:.5}, '-=0.3')  
+        .fromTo(subTitle, 0.6, {top:200, opacity: 0},{top:(itemH - (itemH/3)), opacity:.5}, '-=0.3')
+    }
+
+    init();
+
+    function setupDraggable() { 
+      Draggable.create($carouselItems, {
+        type:'x',
+        edgeResistance: 0.90,
+        dragResistance: 0.0,
+        bounds:'.carousel_container',
+        onDrag:updateDirections,
+        onThrowUpdate:updateDirections,
+        throwProps:true,
+        onDragStart:function(evt) {},
+        onDragEnd :function() {
+
+          if(swipeDir == 'left') {activeID++}
+          else if(swipeDir == 'right') {activeID--};
+          
+          navigateSlide();
+        }
+      });    
+    };
+
+    // set up dots
+    function setupDots() {    
+      for(var i = 0; i < carousel_count; i++) {
+        $navDots.append('<div class="nav_dot" id="dot_' + i + '"></div>');
+      }    
+      $navDot = $('.nav_dot');
+    }
+
+    function updateDirections() {
+      swipeDir = this.getDirection("start");
+    }
+
+    // navigate slide
+    function navigateSlide() {
+    
+      if(activeID >= carousel_count-1) activeID = carousel_count-1;
+      if(activeID <= 0) activeID = 0;   
+              
+      var xTarget = ((activeID * itemW) * -1);
+      TweenMax.to($carouselItems, slideSpeed, {x: xTarget, ease: slideMeth, onComplete: slideDone});
+    }
+
+    function slideDone() {
+      //
+      if(activeID == 0) {$arrowPrev.fadeOut()} 
+      else {$arrowPrev.fadeIn()}
+      
+      if(activeID + 1 == carousel_count) {$arrowNext.fadeOut()}
+      else {$arrowNext.fadeIn()}
+    }
+
+    $itemArrow.on('click', function() {
+    
+      if(Modernizr.touch) return;
+      
+      if($(this).hasClass('item_next')) {activeID++}
+      else {activeID--};
+      
+      navigateSlide();
+    });
+    
+    $itemArrow.on('touchstart', function() {
+      if($(this).hasClass('item_next')) {activeID++}
+      else {activeID--};
+      
+      navigateSlide();
+    });
+
+    $navDot.hover(    
+      function() {      
+          TweenMax.to($(this), .35, {scale: 1.5});
+      }, function() {
+         if($(this).attr('id').split('_')[1] == activeID) return;
+         TweenMax.to($(this), .35, {scale: 1.0});
+      }  
+    );
+  
+    $navDot.click(function() {    
+      var dotID = $(this).attr('id').split('_')[1];
+      activeID = dotID;
+        
+      navigateSlide();    
+    });
+
+    $carouselItem.mousedown(function() {    
+      activeID = $(this).attr('id').split('_')[1];
+      
+      $(this).removeClass('grab');
+      $(this).addClass('grabbing'); 
+    });
+      
+    $carouselItem.mouseenter(function() {        
+      $(this).removeClass('grabbing');
+      $(this).addClass('grab');
+    });
+
+    $carouselItem.mouseup(function() {        
+      $(this).removeClass('grabbing');
+      $(this).addClass('grab');
+    });  
+  })();
+}
---
+++ resources/views/index.blade.php
@@ -0,0 +1,82 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>iWorx Portfolio</title>
+        <!-- css -->
+        <link rel = "stylesheet" href = "http://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.3.0/css/font-awesome.min.css">
+        <link rel="stylesheet" href="css/index.css">
+        <!-- js -->
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.2/jquery.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/1.18.5/TweenMax.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/1.18.5/utils/Draggable.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/1.18.5/plugins/ScrollToPlugin.min.js"></script>
+        <script type="text/javascript" src="js/modernizr-custom.js"></script>
+        <script type="text/javascript" src="js/index.js"></script>
+    </head>
+    <body>
+
+    <div id="main_title">
+        <p>Welcome to iWorx Portfolio</p>
+    </div>
+
+    <div class="left_sidebar">
+            
+    </div>
+
+    <div class="main">
+        <div class="slideshow">
+          <div id="carousel_container" class="carousel_container">
+            <div class="carousel_items">        
+              <ul>
+                <li class="carousel_item" id="item_0">
+                  <img class="image" src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/231648/MB_CLA_1.jpg" />
+                    <h2 class="slideShow__slideTitle">iWorx</h2>
+                    <div class="slideShow__slideSubTitle">- Web Design & Development</div>
+                </li>
+                <li class="carousel_item" id="item_1">
+                  <img class="image" src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/231648/MB_CLA_2.jpg" />
+                </li>
+                <li class="carousel_item" id="item_2">
+                  <img class="image" src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/231648/MB_CLA_3.jpg" />
+                </li>  
+                <li class="carousel_item" id="item_3">
+                  <img class="image" src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/231648/MB_CLA_4.jpg" />
+                </li>    
+                <li class="carousel_item" id="item_4">
+                  <img class="image" src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/231648/MB_CLA_5.jpg" />
+                </li>      
+                <li class="carousel_item" id="item_5">
+                  <img class="image" src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/231648/MB_CLA_6.jpg" />
+                </li>
+                <li class="carousel_item" id="item_6">
+                  <img class="image" src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/231648/MB_CLA_7.jpg" />
+                </li>
+                <li class="carousel_item" id="item_7">
+                  <img class="image" src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/231648/MB_CLA_8.jpg" />
+                </li>  
+                <li class="carousel_item" id="item_8">
+                  <img class="image" src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/231648/MB_CLA_9.jpg" />
+                </li>    
+                <li class="carousel_item" id="item_9">
+                  <img class="image" src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/231648/MB_CLA_10.jpg" />
+                </li>
+              </ul>
+            </div>
+            <div class="item_arrows"> 
+              <div class="item_arrow item_prev">
+                <i class="fa fa-angle-left"></i>
+              </div>  
+              <div class="item_arrow item_next">
+                <i class="fa fa-angle-right"></i>
+              </div>        
+            </div>
+          </div>
+          <div class="nav_dots"></div>
+        </div>
+    </div>
+
+    <div class="right_sidebar"> 
+    </div>
+                    
+    </body>
+</html>
